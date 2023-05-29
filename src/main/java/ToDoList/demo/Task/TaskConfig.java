eb3798060d7f2377040dd3eaea66d7d6c41bce11
Brandon Alfaro <brandonalfaro20@gmail.com>
2023-05-29 00:24:17 -0500

    Prepopulated tasks and finished get requests

--- src/main/java/ToDoList/demo/Task/TaskConfig.java
+++ src/main/java/ToDoList/demo/Task/TaskConfig.java
@@ -1,4 +1,21 @@
 package ToDoList.demo.Task;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class TaskConfig {
+
+    @Bean
+    CommandLineRunner taskCommandLineRunner(TaskRepository taskRepository) {
+        return args -> {
+            Task task1 = new Task("New task", 1, "IT");
+            Task task2 = new Task("Another task", 2, "Tax");
+            taskRepository.saveAll(List.of(task1, task2));
+        };
+    }
+
 }
--- src/main/java/ToDoList/demo/Task/TaskController.java
+++ src/main/java/ToDoList/demo/Task/TaskController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,27 @@ import java.util.List;
 @RequestMapping("/tasks")
 public class TaskController {
 
+
+    private TaskService taskService;
+
     @Autowired
-    TaskRepository taskRepository;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping
     public ResponseEntity<List<Task>> getTasks() {
-        return new ResponseEntity<>(taskRepository.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(taskService.getAllTasks(), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable long id) {
+        return new ResponseEntity<>(taskService.getTaskById(id), HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}/tasks")
+    public ResponseEntity<List<Task>> getTasksByEmployeeId(@PathVariable long id) {
+        return new ResponseEntity<>(taskService.getTasksByEmployeeId(id), HttpStatus.OK);
+    }
+
+
 }
